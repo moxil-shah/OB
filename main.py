cd3ffb3791e814ea1b8e0684b3c1ef727b316c83
Moxil Shah <shahmv200216@gmail.com>
2023-05-11 20:48:13 -0600

    time to add mof

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import plotly.graph_objs as go
 import numpy as np
 import requests
 import json
-
-
+from datetime import datetime, timedelta
 def getOrderBook(symbol, limit):
     response = requests.get(
         f'https://api.binance.com/api/v3/depth?symbol={symbol}&limit={limit}')
@@ -49,55 +48,56 @@ def sumQuantities(bids, asks, bucket_size):
 
     return bid_buckets, ask_buckets
 
+def getColumns(y_min, middle, y_max, bidsDic, asksDic):
+    initColumn = np.array([])
+    for bidPrice in range(y_min, middle):
+        if bidPrice in bidsDic:
+            initColumn = np.append(initColumn, bidsDic[bidPrice])
+        else:
+            initColumn = np.append(initColumn, np.nan)
+    middleVal = 0
+    if middle in bidsDic:
+        middleVal += bidsDic[middle]
+    if middle in asksDic:
+        middleVal += asksDic[middle]
+    initColumn = np.append(initColumn, middleVal)
+    for askPrice in range(middle + 1, y_max + 1):
+        if askPrice in asksDic:
+            initColumn = np.append(initColumn, asksDic[askPrice])
+        else:
+            initColumn = np.append(initColumn, np.nan)
+    return initColumn
 
 # Initialize the app
 app = dash.Dash(__name__)
-bucketSize = 1
-orderBookSize = 5000
-priceLevels = 201
-maxColumns = 100
-bothSides = 100
-middle = getPriceOfAssetAdjustedForBucketSize("ETHUSDT", bucketSize)
-obJSON = getOrderBook("ETHUSDT", orderBookSize)
-bidsDic, asksDic = sumQuantities(obJSON["bids"], obJSON["asks"], bucketSize)
-bidsPricesList = list(bidsDic.keys())
-asksPricesList = list(asksDic.keys())
-bidsPricesList.sort()
-asksPricesList.sort()
+BUCKETSIZE = 1
+ORDERBOOKSIZE = 5000
+PRICELEVELS = 201
+MAXCOLUMNS = 100
+BOTHSIDES = 100
+INTERVAL = 3000
+middle = getPriceOfAssetAdjustedForBucketSize("ETHUSDT", BUCKETSIZE)
 
 # Set the initial y-axis range
-y_min = middle - bothSides
-y_max = middle + bothSides
-
+y_min = middle - BOTHSIDES
+y_max = middle + BOTHSIDES
 
 # Create an empty heatmap with random values
-heatmap = np.full((priceLevels, maxColumns), np.nan)
-initColumn = np.array([])
-for bidPrice in range(y_min, middle):
-    if bidPrice in bidsDic:
-        initColumn = np.append(initColumn, bidsDic[bidPrice])
-    else:
-        initColumn = np.append(initColumn, np.nan)
-middleVal = 0
-if middle in bidsDic:
-    middleVal += bidsDic[middle]
-if middle in asksDic:
-    middleVal += asksDic[middle]
-initColumn = np.append(initColumn, middleVal)
-for askPrice in range(middle + 1, y_max + 1):
-    if askPrice in asksDic:
-        initColumn = np.append(initColumn, asksDic[askPrice])
-    else:
-        initColumn = np.append(initColumn, np.nan)
-# Replace the last column of the array with the custom column
-heatmap[:, -1] = initColumn
+heatmap = np.full((PRICELEVELS, MAXCOLUMNS), np.nan)
+
+timeArray = np.array([])
+now = datetime.now() - timedelta(seconds=INTERVAL // 1000 * MAXCOLUMNS)
+for i in range(MAXCOLUMNS):
+    timeArray = np.append(timeArray, now)
+    now = now + timedelta(seconds=INTERVAL // 1000)
+
 # Create the trace for the heatmap
-trace = go.Heatmap(z=heatmap, y=np.arange(y_min, y_max+1, 1), colorscale='hot')
+trace = go.Heatmap(z=heatmap, x=timeArray, y=np.arange(y_min, y_max+1, 1), colorscale='hot')
 
 # Create the layout for the heatmap
 layout = go.Layout(
     title='Real-Time Order Book',
-    xaxis=dict(title='X Axis'),
+    xaxis=dict(title='Time'),
     yaxis=dict(title='Price'),
     height=800
 )
@@ -108,7 +108,7 @@ fig = go.Figure(data=[trace], layout=layout)
 # Create the app layout
 app.layout = html.Div(children=[
     dcc.Graph(id='realtime-orderbook', figure=fig),
-    dcc.Interval(id='interval-component', interval=4000, n_intervals=0)
+    dcc.Interval(id='interval-component', interval=INTERVAL, n_intervals=0)
 ])
 
 # Define the callback function to update the heatmap
@@ -118,20 +118,16 @@ app.layout = html.Div(children=[
     prevent_initial_call=True
 )
 def update_heatmap(n):
-    global y_min, y_max, heatmap
+    global y_min, y_max, heatmap, timeArray
     # Generate new column of heatmap data
-    middle = getPriceOfAssetAdjustedForBucketSize("ETHUSDT", bucketSize)
-    obJSON = getOrderBook("ETHUSDT", orderBookSize)
+    middle = getPriceOfAssetAdjustedForBucketSize("ETHUSDT", BUCKETSIZE)
+    obJSON = getOrderBook("ETHUSDT", ORDERBOOKSIZE)
     bidsDic, asksDic = sumQuantities(
-        obJSON["bids"], obJSON["asks"], bucketSize)
-    bidsPricesList = list(bidsDic.keys())
-    asksPricesList = list(asksDic.keys())
-    bidsPricesList.sort()
-    asksPricesList.sort()
+        obJSON["bids"], obJSON["asks"], BUCKETSIZE)
 
     # Set the initial y-axis range
-    y_min_new = middle - bothSides
-    y_max_new = middle + bothSides
+    y_min_new = middle - BOTHSIDES
+    y_max_new = middle + BOTHSIDES
 
     if y_max_new > y_max:
         shiftUp = y_max_new - y_max
@@ -149,36 +145,19 @@ def update_heatmap(n):
         # concatenate the NaN rows with the heatmap along the row axis
         heatmap = np.concatenate((nan_rows, heatmap), axis=0)
 
-    new_col = np.array([])
-    for bidPrice in range(y_min_new, middle): 
-        if bidPrice in bidsDic:
-            new_col = np.append(new_col, bidsDic[bidPrice])
-        else:
-            new_col = np.append(new_col, np.nan)
-    middleVal = 0
-    if middle in bidsDic:
-        middleVal += bidsDic[middle]
-    if middle in asksDic:
-        middleVal += asksDic[middle]
-    new_col = np.append(new_col, middleVal)
-    for askPrice in range(middle + 1, y_max_new + 1):
-        if askPrice in asksDic:
-            new_col = np.append(new_col, asksDic[askPrice])
-        else:
-            new_col = np.append(new_col, np.nan)
+    new_col = getColumns(y_min_new, middle, y_max_new, bidsDic, asksDic)
+
     new_col = new_col.reshape((len(new_col), 1))
 
     # Add new column to existing heatmap data
     heatmap = np.concatenate((heatmap, new_col), axis=1)
 
-    # Check if heatmap has more than 100 columns
-    if heatmap.shape[1] > maxColumns:
-
-        # Delete the leftmost column of heatmap data
-        heatmap = heatmap[:, 1:]
-
+    # Delete the leftmost column of heatmap data
+    heatmap = heatmap[:, 1:]
+    timeArray = timeArray[1:]
+    timeArray = np.append(timeArray, datetime.now())
     # Update the heatmap trace with the new data and y-axis range
-    fig.data[0].update(z=heatmap, y=np.arange(y_min_new, y_max_new+1, 1))
+    fig.data[0].update(z=heatmap, x=timeArray, y=np.arange(y_min_new, y_max_new+1, 1))
 
     # Store the updated y-axis range for the next update
     y_min = y_min_new
