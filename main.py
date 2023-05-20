a65a7156ee50daae1ba0c082e6dfcf17189edef1
Moxil Shah <shahmv200216@gmail.com>
2023-05-20 15:53:32 -0600

    added mof

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import numpy as np
 import plotly.graph_objs as go
 import requests
 from datetime import datetime, timedelta
+import threading
+import websocket
+import json
 from dash import Dash, dcc, html, ctx
 from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
 
+### CONSTANTS ###
 VERIFY = True
 BUCKETSIZE = 1
+
+### FUNCTIONS ###
 
 
 def getOrderBook(symbol, limit):
@@ -103,6 +110,66 @@ def padTimeArray():
         now = now + timedelta(seconds=g_intervals // 1000)
 
 
+def on_message(ws, message):
+    global g_marketOrderFlowX, g_marketOrderFlowY, g_bubbleSizes
+    try:
+        jsonData = json.loads(message)
+        symbol = jsonData["s"]
+        price = jsonData["p"]
+        quantity = jsonData["q"]
+        tradeTime = jsonData["T"]
+        isBuyerMarketMaker = jsonData["m"]
+    except json.JSONDecodeError as e:
+        print("Error decoding JSON:", str(e))
+    except KeyError as e:
+        print("Key not found in JSON:", str(e))
+    except Exception as e:
+        print("An error occurred:", str(e))
+    else:
+        tradeTimeProcessed = datetime.fromtimestamp(int(tradeTime) / 1000)
+        if len(g_marketOrderFlowX) and tradeTimeProcessed <= g_marketOrderFlowX[-1]:
+            # Add millisecond pad to the current time
+            tradeTimeProcessed += g_marketOrderFlowX[-1] - \
+                tradeTimeProcessed + timedelta(milliseconds=1)
+
+        g_marketOrderFlowX = np.append(g_marketOrderFlowX, tradeTimeProcessed)
+        g_marketOrderFlowY = np.append(g_marketOrderFlowY, float(price))
+        g_bubbleSizes = np.append(g_bubbleSizes, float(quantity))
+        colors = np.where(isBuyerMarketMaker is True,
+                          'rgb(255, 0, 0)', 'rgb(0, 255, 0)')
+        if fig['data'][3]['marker']['color'] is None:
+            fig['data'][3]['marker']['color'] = []
+        newColors = np.append(fig['data'][3]['marker']['color'], colors)
+
+        while len(g_marketOrderFlowX) > 0 and g_marketOrderFlowX[0] < g_timeArray[0]:
+            g_marketOrderFlowX = np.delete(g_marketOrderFlowX, 0)
+            g_marketOrderFlowY = np.delete(g_marketOrderFlowY, 0)
+            g_bubbleSizes = np.delete(g_bubbleSizes, 0)
+            newColors = np.delete(newColors, 0)
+
+        fig['data'][3]['x'] = g_marketOrderFlowX
+        fig['data'][3]['marker']['size'] = g_bubbleSizes
+        fig['data'][3]['y'] = g_marketOrderFlowY
+        fig['data'][3]['marker']['color'] = newColors
+
+
+def on_error(ws, error):
+    # Handle WebSocket errors
+    pass
+
+
+def on_close(ws):
+    # Handle WebSocket closure
+    pass
+
+
+def wsrun(uri):
+    global g_tradeWS
+    g_tradeWS = websocket.WebSocketApp(
+        uri, on_message=on_message, on_error=on_error,  on_close=on_close)
+    g_tradeWS.run_forever()
+
+
 app = Dash(__name__)
 app.prevent_initial_callbacks = False
 
@@ -116,17 +183,30 @@ g_bestBidX = np.empty(0)
 g_bestBidY = np.empty(0)
 g_bestAskX = np.empty(0)
 g_bestAskY = np.empty(0)
+g_marketOrderFlowX = np.empty(0)
+g_marketOrderFlowY = np.empty(0)
+g_endpoints = {
+    'ETH/USDT': 'wss://stream.binance.com:9443/ws/ethusdt@aggTrade',
+    'BTC/USDT': 'wss://stream.binance.com:9443/ws/btcusdt@aggTrade'
+}
+g_bubbleSizes = np.empty(0)
+g_tradeWS = None
+g_updateHeatmapBusy = False
 
 padTimeArray()
 
 # Create the trace for the heatmap
 heatTrace = go.Heatmap(name='Limit Orders', z=g_heatmap, x=g_timeArray, y=np.arange(
     g_yMin, g_yMax+1, 1), colorscale='Cividis')
-
 bestBidTrace = go.Scatter(x=g_bestBidX, y=g_bestBidY, mode=g_initialMode,
                           name='Best Bid Trace', line=dict(width=1, color='green'))
 bestAskTrace = go.Scatter(x=g_bestAskX, y=g_bestAskY, mode=g_initialMode,
                           name='Best Ask Trace', line=dict(width=1, color='red'))
+bubbleTrace = go.Scatter(x=g_marketOrderFlowX, y=g_marketOrderFlowY, mode=g_initialMode, name='Volume Profile', line=dict(width=2, color='white'),
+                         marker=dict(size=g_bubbleSizes,
+                                     sizemode='diameter', sizeref=1),
+                         hovertemplate="Quantity: %{marker.size:.2f}<br>Price: %{y}<extra></extra>"
+                         )
 
 # Create the layout for the heatmap
 layout = go.Layout(
@@ -137,106 +217,110 @@ layout = go.Layout(
 )
 
 # Create the figure with the trace and layout
-fig = go.Figure(data=[heatTrace, bestBidTrace, bestAskTrace], layout=layout)
+fig = go.Figure(data=[heatTrace, bestBidTrace,
+                bestAskTrace, bubbleTrace], layout=layout)
 fig['layout']['uirevision'] = 1
 
 # Create the app layout
 app.layout = html.Div(children=[
-    dcc.Dropdown(['BTCUSDT', 'ETHUSDT'],
-                 'ETHUSDT', id='pair-dropdown'),
+    dcc.Dropdown(
+        id='endpoint-dropdown',
+        options=[{'label': symbol, 'value': uri}
+                 for symbol, uri in g_endpoints.items()],
+        # Set the default value to the first endpoint
+        value=None
+    ),
     dcc.Interval(id='interval-component',
                  interval=g_intervals, n_intervals=0),
-    dcc.RadioItems(
-        id='mode-switch',
-        options=[
-            {'label': 'Lines', 'value': 'lines'},
-            {'label': 'Markers', 'value': 'markers'},
-            {'label': 'Lines+Markers', 'value': 'lines+markers'}
-        ],
-        value=g_initialMode
-    ),
-    dcc.Graph(id='realtime-orderbook', figure=fig)])
+    dcc.Graph(id='realtime-orderbook', figure=fig), html.Div(id='output')])
+
+# Callback to update the WebSocket connection when the dropdown value changes
+
+
+@app.callback(Output('output', 'children'), [Input('endpoint-dropdown', 'value')], prevent_initial_call=True)
+def update_websocket(uri):
+    if g_tradeWS:
+        g_tradeWS.close()
+    websocket_thread = threading.Thread(
+        target=wsrun, args=(uri,), name='websocket_thread')
+    websocket_thread.start()
+
+    return f"Connected to {uri}"
 
 
 @app.callback(
     Output('realtime-orderbook', 'figure'),
     Input('interval-component', 'n_intervals'),
-    Input('mode-switch', 'value'),
 )
-def update_heatmap(n, modeValue):
-    global g_yMin, g_yMax, g_heatmap, g_timeArray, g_bestBidX, g_bestBidY, g_bestAskX, g_bestAskY, g_orderBookSize
-    triggered_id = ctx.triggered_id
+def update_heatmap(n):
+    global g_yMin, g_yMax, g_heatmap, g_timeArray, g_bestBidX, g_bestBidY, g_bestAskX, g_bestAskY, g_orderBookSize, g_marketOrderFlowX, g_marketOrderFlowY, g_bubbleSizes, g_updateHeatmapBusy
 
-    if triggered_id == "mode-switch":
-        fig['data'][1].mode = modeValue
+    columnTime = datetime.now()
+    try:
+        middle = getPriceOfAssetAdjustedForBucketSize(
+            g_tradingPair, BUCKETSIZE)
+        obJSON = getOrderBook(g_tradingPair, g_orderBookSize)
+        bestBid = obJSON["bids"][0][0]
+        bestAsk = obJSON["asks"][0][0]
+    except Exception as e:
+        print("Error: ", e)
+        return fig
+    else:
+        bidsDic, asksDic = sumQuantities(
+            obJSON["bids"], obJSON["asks"], BUCKETSIZE)
 
-    elif triggered_id == "interval-component":
-        columnTime = datetime.now()
-        try:
-            middle = getPriceOfAssetAdjustedForBucketSize(
-                g_tradingPair, BUCKETSIZE)
-            obJSON = getOrderBook(g_tradingPair, g_orderBookSize)
-            bestBid = obJSON["bids"][0][0]
-            bestAsk = obJSON["asks"][0][0]
-        except Exception as e:
-            print("Error: ", e)
-            return fig
-        else:
-            bidsDic, asksDic = sumQuantities(
-                obJSON["bids"], obJSON["asks"], BUCKETSIZE)
+    # Set the initial y-axis range
+    yMinNew = middle - g_bothSides
+    yMaxNew = middle + g_bothSides
 
-        # Set the initial y-axis range
-        yMinNew = middle - g_bothSides
-        yMaxNew = middle + g_bothSides
+    if yMaxNew > g_yMax:
+        shiftUp = yMaxNew - g_yMax
+        g_heatmap = g_heatmap[shiftUp:, :]
+        # create a 2D array of NaN values with n rows and the same number of columns as the heatmap
+        nan_rows = np.full((shiftUp, g_heatmap.shape[1]), np.nan)
+        # concatenate the NaN rows with the heatmap along the row axis
+        g_heatmap = np.concatenate((g_heatmap, nan_rows), axis=0)
+    elif yMaxNew < g_yMax:
+        shiftDown = g_yMax - yMaxNew
+        g_heatmap = g_heatmap[:-shiftDown, :]
+        # create a 2D array of NaN values with n rows and the same number of columns as the heatmap
+        nan_rows = np.full((shiftDown, g_heatmap.shape[1]), np.nan)
+        # concatenate the NaN rows with the heatmap along the row axis
+        g_heatmap = np.concatenate((nan_rows, g_heatmap), axis=0)
 
-        if yMaxNew > g_yMax:
-            shiftUp = yMaxNew - g_yMax
-            g_heatmap = g_heatmap[shiftUp:, :]
-            # create a 2D array of NaN values with n rows and the same number of columns as the heatmap
-            nan_rows = np.full((shiftUp, g_heatmap.shape[1]), np.nan)
-            # concatenate the NaN rows with the heatmap along the row axis
-            g_heatmap = np.concatenate((g_heatmap, nan_rows), axis=0)
-        elif yMaxNew < g_yMax:
-            shiftDown = g_yMax - yMaxNew
-            g_heatmap = g_heatmap[:-shiftDown, :]
-            # create a 2D array of NaN values with n rows and the same number of columns as the heatmap
-            nan_rows = np.full((shiftDown, g_heatmap.shape[1]), np.nan)
-            # concatenate the NaN rows with the heatmap along the row axis
-            g_heatmap = np.concatenate((nan_rows, g_heatmap), axis=0)
+    # Generate new column of heatmap data
+    new_col = getColumns(yMinNew, middle, yMaxNew, bidsDic, asksDic)
+    new_col = new_col.reshape((len(new_col), 1))
 
-        # Generate new column of heatmap data
-        new_col = getColumns(yMinNew, middle, yMaxNew, bidsDic, asksDic)
-        new_col = new_col.reshape((len(new_col), 1))
+    # Add new column to existing heatmap data
+    g_heatmap = np.concatenate((g_heatmap, new_col), axis=1)
 
-        # Add new column to existing heatmap data
-        g_heatmap = np.concatenate((g_heatmap, new_col), axis=1)
+    g_bestBidX = np.append(g_bestBidX,  columnTime)
+    g_bestBidY = np.append(g_bestBidY, bestBid)
+    g_bestAskX = np.append(g_bestAskX,  columnTime)
+    g_bestAskY = np.append(g_bestAskY, bestAsk)
+    # Delete the leftmost column of heatmap data
+    g_heatmap = g_heatmap[:, 1:]
 
-        g_bestBidX = np.append(g_bestBidX,  columnTime)
-        g_bestBidY = np.append(g_bestBidY, bestBid)
-        g_bestAskX = np.append(g_bestAskX,  columnTime)
-        g_bestAskY = np.append(g_bestAskY, bestAsk)
-        # Delete the leftmost column of heatmap data
-        g_heatmap = g_heatmap[:, 1:]
+    if len(g_bestBidX) > g_maxColumns:
+        g_bestBidX = np.delete(g_bestBidX, 0)
+        g_bestBidY = np.delete(g_bestBidY, 0)
+    if len(g_bestAskX) > g_maxColumns:
+        g_bestAskX = np.delete(g_bestAskX, 0)
+        g_bestAskY = np.delete(g_bestAskY, 0)
 
-        if len(g_bestBidX) > g_maxColumns:
-            g_bestBidX = np.delete(g_bestBidX, 0)
-            g_bestBidY = np.delete(g_bestBidY, 0)
-        if len(g_bestAskX) > g_maxColumns:
-            g_bestAskX = np.delete(g_bestAskX, 0)
-            g_bestAskY = np.delete(g_bestAskY, 0)
+    g_timeArray = np.append(g_timeArray[1:], columnTime)
+    # Update the heatmap trace with the new data and y-axis range
+    fig.data[0].update(z=g_heatmap, x=g_timeArray,
+                       y=np.arange(yMinNew, yMaxNew+1, 1))
+    fig['data'][1]['x'] = g_bestBidX
+    fig['data'][1]['y'] = g_bestBidY
+    fig['data'][2]['x'] = g_bestAskX
+    fig['data'][2]['y'] = g_bestAskY
 
-        g_timeArray = np.append(g_timeArray[1:], columnTime)
-        # Update the heatmap trace with the new data and y-axis range
-        fig.data[0].update(z=g_heatmap, x=g_timeArray,
-                           y=np.arange(yMinNew, yMaxNew+1, 1))
-        fig['data'][1]['x'] = g_bestBidX
-        fig['data'][1]['y'] = g_bestBidY
-        fig['data'][2]['x'] = g_bestAskX
-        fig['data'][2]['y'] = g_bestAskY
-
-        # Store the updated y-axis range for the next update
-        g_yMin = yMinNew
-        g_yMax = yMaxNew
+    # Store the updated y-axis range for the next update
+    g_yMin = yMinNew
+    g_yMax = yMaxNew
 
     # Return the updated figure
     return fig
--- test.py
+++ test.py
@@ -1,14 +1,56 @@
-import requests
+import threading
+import websocket
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
+import time
+# Global flag to control the WebSocket thread
+ws = None
 
-def getOrderBook(symbol, limit):
-    url = f'https://api.binance.com/api/v3/depth?symbol={symbol}&limit={limit}'
-    response = requests.get(url, verify=False)
-    response.raise_for_status()  # Check for request failure and raise an exception if needed
-    response_json = response.json()
-    if "bids" not in response_json or "asks" not in response_json:
-        raise Exception("Orderbook request failed")
-    return response_json
+# Function to handle the WebSocket connection and print received messages
+def wsrun(uri):
+    global ws
+    ws = websocket.WebSocketApp(uri, on_message=on_message)
+    ws.run_forever()
 
-while True:
-    print(getOrderBook("ETHUSDT", 5000))
-    
+
+def on_message(ws, message):
+    print(message)
+
+# Define the Dash app
+app = dash.Dash(__name__)
+
+# Define the available WebSocket endpoints
+endpoints = {
+    'ETH/USDT': 'wss://stream.binance.com:9443/ws/ethusdt@trade',
+    'BTC/USDT': 'wss://stream.binance.com:9443/ws/btcusdt@trade'
+}
+
+# Define the layout of the app
+app.layout = html.Div([
+    html.H1('WebSocket Endpoint Selector'),
+    dcc.Dropdown(
+        id='endpoint-dropdown',
+        options=[{'label': symbol, 'value': uri} for symbol, uri in endpoints.items()],
+        value=list(endpoints.values())[0]  # Set the default value to the first endpoint
+    ),
+    html.Div(id='output')
+])
+
+# Callback to update the WebSocket connection when the dropdown value changes
+@app.callback(Output('output', 'children'), [Input('endpoint-dropdown', 'value')])
+def update_websocket(uri):
+    if uri is None:
+        raise PreventUpdate
+    else:
+        ws.close()
+    websocket_thread = threading.Thread(target=wsrun, args=(uri,), name='websocket_thread')
+    websocket_thread.start()
+
+    return f"Connected to {uri}"
+
+# Run the Dash app
+if __name__ == '__main__':
+    app.run_server(debug=True)
